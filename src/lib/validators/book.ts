17e249218606429a194424494f233142646afd4f
altintasutku <altintasutku@gmail.com>
2024-05-01 22:54:24 +0300

    typing

--- src/lib/validators/book.ts
+++ src/lib/validators/book.ts
@@ -1,21 +1,23 @@
 import { z } from "zod";
 
+export const categoryValidator = z.object({
+  name: z.string().max(64, "Category name must be less than 64 characters."),
+});
+
 export const uploadBookStateValidator = z.object({
-  infos: z.object({
-    title: z.string().max(256, "Title must be less than 256 characters."),
-    authorId: z.string(),
-    description: z
-      .string()
-      .max(1024, "Description must be less than 1024 characters."),
-    price: z.number(),
-    banner: z.string(),
-    discount: z.number().min(0).max(100),
-    pdf: z.string(),
-    cover: z.string(),
-    currency: z.string().max(3, "Currency must be less than 3 characters."),
-    pageCount: z.number(),
-  }),
+  title: z.string().max(256, "Title must be less than 256 characters."),
+  authorId: z.string(),
+  description: z
+    .string()
+    .max(1024, "Description must be less than 1024 characters."),
+  price: z.number(),
+  banner: z.string(),
+  discount: z.number().min(0).max(100),
+  pdf: z.string(),
+  cover: z.string(),
+  currency: z.string().max(3, "Currency must be less than 3 characters."),
+  pageCount: z.number(),
   categories: z
-    .array(z.object({ name: z.string() }))
+    .array(categoryValidator)
     .max(5, "You can add up to 5 categories."),
 });
